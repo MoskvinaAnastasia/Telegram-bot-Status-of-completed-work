6a4378e1a2ed3645fb0155cb8c2836ac4ef2b5b9
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-04-29 00:19:39 +0300

    new

--- homework.py
+++ homework.py
@@ -1,9 +1,9 @@
+import json
 import logging
 import os
 import sys
 import time
 from http import HTTPStatus
-
 
 from dotenv import load_dotenv
 import requests
@@ -11,14 +11,6 @@ import telegram
 
 
 load_dotenv()
-
-logging.basicConfig(
-    handlers=[logging.StreamHandler(stream=sys.stdout),
-              logging.FileHandler('logging_bot.log')
-              ],
-    level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -35,17 +27,29 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_FILE_PATH = os.path.join(SCRIPT_DIR, 'logging_bot.log')
 
-def check_tokens() -> bool:
-    """Проверяем доступность переменных окружения."""
-    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
+
+def check_tokens() -> list[str]:
+    """
+    Проверяем доступность переменных окружения.
+    И возвращаем список отсутствующих.
+    """
+    missing_tokens = []
+    if not PRACTICUM_TOKEN:
+        missing_tokens.append('PRACTICUM_TOKEN')
+    if not TELEGRAM_TOKEN:
+        missing_tokens.append('TELEGRAM_TOKEN')
+    if not TELEGRAM_CHAT_ID:
+        missing_tokens.append('TELEGRAM_CHAT_ID')
+    return missing_tokens
 
 
 def send_message(bot: telegram.Bot, message: str) -> bool:
     """Отправляет сообщение в Telegram чат."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message,)
-        logging.debug(f"Сообщение отправлено в Telegram: {message}")
     except telegram.TelegramError as error:
         logging.error(f"Ошибка при отправке сообщения в Telegram: {error}")
     else:
@@ -57,26 +61,37 @@ def get_api_answer(timestamp: int) -> dict:
     payload = {'from_date': timestamp}
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=payload)
+        if response.status_code != HTTPStatus.OK:
+            raise RuntimeError(f'Код ответа: {response.status_code}')
+        return response.json()
     except requests.RequestException as error:
         raise RuntimeError(f'Произошла ошибка при запросе к API: {error}')
-    if response.status_code != HTTPStatus.OK:
-        raise RuntimeError(f'Код ответа: {response.status_code}')
-    try:
-        return response.json()
-    except ValueError:
-        raise RuntimeError('Формат не соответствует json')
+    except json.JSONDecodeError as decode_error:
+        raise RuntimeError(f'Ошибка при парсинге JSON данных: {decode_error}')
 
 
 def check_response(response: dict) -> list:
     """Проверяет ответ API на соответствие документации."""
     if not isinstance(response, dict):
-        logging.error('Ответ API должен быть в виде словаря')
         raise TypeError('Ответ API должен быть в виде словаря')
-    if not isinstance(response.get('homeworks'), list):
-        raise TypeError('homeworks должен быть в виде списка')
-    if not isinstance(response.get('current_date'), int):
-        raise TypeError('current_date должен быть в виде числа')
-    return response['homeworks']
+    homeworks = response.get('homeworks')
+    if homeworks is None:
+        logging.error('Отсутствует ключ "homeworks" в ответе API')
+        raise KeyError('Отсутствует ключ "homeworks" в ответе API')
+    if not isinstance(homeworks, list):
+        logging.error('Значение ключа "homeworks" должно быть '
+                      'представлено в виде списка')
+        raise TypeError('Значение ключа "homeworks" должно быть '
+                        'представлено в виде списка')
+    current_date = response.get('current_date')
+    if current_date is None:
+        logging.debug('Отсутствует ключ "current_date" в ответе API')
+    if not isinstance(current_date, int):
+        logging.error('Значение ключа "current_date" должно быть '
+                      'представлено в виде числа')
+        raise TypeError('Значение ключа "current_date" должно быть '
+                        'представлено в виде числа')
+    return homeworks
 
 
 def parse_status(homework: dict) -> str:
@@ -103,13 +118,16 @@ def parse_status(homework: dict) -> str:
 
 def main():
     """Основная логика работы бота."""
-    if not check_tokens():
-        message = 'Программа не будет работать. Недостаточно токенов.'
+    missing_tokens = check_tokens()
+    if missing_tokens:
+        missing_tokens_str = ', '.join(missing_tokens)
+        message = (f'Программа не будет работать. '
+                   f'Отсутствуют токены: {missing_tokens_str}')
         logging.critical(message)
         sys.exit(message)
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time())
+    timestamp = int(time.time()) - 2592000
     last_message = ""
 
     while True:
@@ -135,13 +153,19 @@ def main():
                     last_message = message
                 except Exception as send_error:
                     logging.error(
-                        f'Ошибка при отправке сообщения: {send_error}')
-                finally:
+                        f'Ошибка при отправке сообщения: {message}, '
+                        f'ошибка: {send_error}')
                     last_message = message
 
         finally:
             time.sleep(RETRY_PERIOD)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    logging.basicConfig(
+        handlers=[logging.StreamHandler(stream=sys.stdout),
+                  logging.FileHandler(LOG_FILE_PATH)
+                  ],
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
